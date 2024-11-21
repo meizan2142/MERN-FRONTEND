7bb86a6b88797f06426219998bccc1789aebbb3a
Your Name01~git <saifsahai2104@gmail.com>
2024-11-21 09:46:09 +0600

    Password validation done both login and register page

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,14 +1,23 @@
+import { useForm } from "react-hook-form"
 import { FaArrowAltCircleLeft } from "react-icons/fa"
 import { NavLink } from "react-router-dom"
 
 export const LogIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
         <div className="mx-auto w-full max-w-md space-y-4 rounded-lg border bg-white p-10 my-36">
             <div className='text-3xl'>
                 <NavLink to='/'><FaArrowAltCircleLeft /></NavLink>
             </div>
             <h1 className="text-3xl font-semibold">Sign In</h1>
-            <form className="space-y-6">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="Email" className="block font-bold text-black">
                         Email
@@ -18,7 +27,11 @@ export const LogIn = () => {
                         id="Email"
                         placeholder="Enter email"
                         type="text"
+                        {...register('email', { 'required': true })}
                     />
+                    {errors.email?.type === 'required' && (
+                        <p className='text-red-400'>Email is required</p>
+                    )}
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="password" className="block  font-bold text-black">
@@ -28,7 +41,22 @@ export const LogIn = () => {
                         className="flex h-10 w-full rounded-md border px-3 py-2 text-sm focus:ring-1 text-black focus-visible:outline-none dark:border-zinc-700"
                         placeholder="Enter Password"
                         type="password"
+                        {...register("password", {
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be at least 8 characters",
+                            },
+                            pattern: {
+                                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                message:
+                                    "Password must include uppercase, lowercase, a number, and a special character",
+                            },
+                        })}
                     />
+                    {errors.password && (
+                        <p style={{ color: "red" }}>{errors.password.message}</p>
+                    )}
                 </div>
                 <button className="w-1/2  rounded-md bg-cyan-500 mx-[90px] px-4 py-2 text-white transition-colors ">Sign In</button>
             </form>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,23 @@
-import {NavLink} from 'react-router-dom'
+import { useForm } from "react-hook-form"
+import { NavLink } from 'react-router-dom'
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 export const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
         <div className="mx-auto w-full max-w-md space-y-4 rounded-lg border bg-white p-10 my-36">
             <div className='text-3xl'>
                 <NavLink to='/'><FaArrowAltCircleLeft /></NavLink>
             </div>
             <h1 className="text-3xl font-semibold">Create Account</h1>
-            <form className="space-y-6">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="Name" className="block font-bold text-black">
                         Name
@@ -17,7 +27,11 @@ export const Register = () => {
                         id="name"
                         placeholder="Enter you name"
                         type="text"
+                        {...register('name', { 'required': true })}
                     />
+                    {errors.name?.type === 'required' && (
+                        <p className='text-red-400'>Name is required</p>
+                    )}
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="image" className="block font-bold text-black">
@@ -28,7 +42,11 @@ export const Register = () => {
                         id="image"
                         placeholder="Enter you PhotoURL"
                         type="file"
+                        {...register('image', { 'required': true })}
                     />
+                    {errors.name?.type === 'required' && (
+                        <p className='text-red-400'>Image is required</p>
+                    )}
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="Email" className="block font-bold text-black">
@@ -39,7 +57,11 @@ export const Register = () => {
                         id="Email"
                         placeholder="Enter email"
                         type="text"
+                        {...register('email', { 'required': true })}
                     />
+                    {errors.email?.type === 'required' && (
+                        <p className='text-red-400'>Email is required</p>
+                    )}
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="password" className="block font-bold text-black">
@@ -49,7 +71,22 @@ export const Register = () => {
                         className="flex h-10 w-full rounded-md border px-3 py-2 text-sm focus:ring-1 text-black focus-visible:outline-none dark:border-zinc-700"
                         placeholder="Enter Password"
                         type="password"
+                        {...register("password", {
+                            required: "Password is required",
+                            minLength: {
+                                value: 8,
+                                message: "Password must be at least 8 characters",
+                            },
+                            pattern: {
+                                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                message:
+                                    "Password must include uppercase, lowercase, a number, and a special character",
+                            },
+                        })}
                     />
+                    {errors.password && (
+                        <p style={{ color: "red" }}>{errors.password.message}</p>
+                    )}
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="password" className="block font-bold text-black">
@@ -60,7 +97,20 @@ export const Register = () => {
                         placeholder="Enter Password"
                         id='confirm'
                         type="password"
+                        {...register('confirm', {
+                            required: true,
+                            validate: (value) => {
+                                if (watch('password') != value) {
+                                    return 'Your password do not match';
+                                }
+                            }
+                        })}
                     />
+                    {
+                        errors?.confirm && (
+                            <p className='text-red-400'>Both passwords must match</p>
+                        )
+                    }
                 </div>
                 <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-300">
                     <label htmlFor="select" className="block font-bold text-black">
@@ -69,6 +119,7 @@ export const Register = () => {
                     <select
                         className="flex h-10 w-full rounded-md border px-3 py-2 text-sm focus:ring-1 text-black focus-visible:outline-none dark:border-zinc-700"
                         id="select"
+                        {...register('select', { required: true })}
                     >
                         <option value="buyer">Buyer</option>
                         <option value="seller">Seller</option>

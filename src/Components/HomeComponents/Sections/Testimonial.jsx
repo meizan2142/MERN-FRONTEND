30e24c95cd0b7e12a9408d2b77437731bc4d8325
Your Name01~git <saifsahai2104@gmail.com>
2024-11-22 13:06:49 +0600

    Testimonial section is created successfully

---
+++ src/Components/HomeComponents/Sections/Testimonial.jsx
@@ -0,0 +1,56 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const cards = [
+    {
+        img: 'https://i.ibb.co.com/zmNDdJp/young-bearded-man-with-striped-shirt-273609-5677.jpg',
+        name: 'Solo Sikao',
+        description: 'A testimonial section highlights positive feedback from clients or users to build trust and credibility.'
+    },
+    {
+        img: 'https://i.ibb.co.com/Q8rGcZg/portrait-smiling-young-man-rubbing-his-hands-171337-10297.jpg',
+        name: 'Bruno Fernandez',
+        description: 'The product quality exceeded my expectations, and the delivery was super fast.'
+    },
+    {
+        img: 'https://i.ibb.co.com/xzpgRrg/portrait-cheerful-confident-young-handsome-man-with-beard-smiling-crossed-arms-176420-12119.jpg',
+        name: 'Harry Maguire',
+        description: 'I love the seamless checkout process and the excellent customer support – highly recommend.'
+    },
+]
+
+const Testimonial = () => {
+    return (
+        <div className="grid desktop:grid-cols-3 desktop:mx-60 laptopL:grid-cols-3 laptopL:mx-5 lg:grid-cols-2 lg:gap-5 md:grid-cols-2 desktop:space-y-0 md:gap-4 mobileL:space-y-4 mobileM:space-y-4 mobileS:space-y-4">
+            {
+                cards.map(card => <>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 200 }}
+                            image={card.img}
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {card.name}
+                            </Typography>
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                {card.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Share</Button>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
+                </>)
+            }
+        </div>
+    )
+}
+
+export default Testimonial
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from "../../Components/HomeComponents/Hero"
 import FeaturedSection from "../../Components/HomeComponents/Sections/FeaturedSection"
+import Testimonial from "../../Components/HomeComponents/Sections/Testimonial"
 
 export const Home = () => {
   return (
@@ -7,9 +8,13 @@ export const Home = () => {
       <div>
         <Hero />
       </div>
-      <div className="fourK:my-24 fourK:mx-10 md:mx-10 lg:mx-36 mobileS:mx-2 mobileM:mx-5 mobileL:mx-9">
+      <div className=" md:mx-10 lg:mx-36 mobileS:mx-2 mobileM:mx-5 mobileL:mx-9">
         <h1 className="my-16 text-4xl font-bold text-center">Fetured Products</h1>
         <FeaturedSection />
+      </div>
+      <div className="md:mx-10 lg:mx-36 mobileS:mx-2 mobileM:mx-5 mobileL:mx-9 desktop:mt-24">
+        <h1 className="my-16 text-4xl font-bold text-center">Testimonial</h1>
+        <Testimonial/>
       </div>
     </div>
   )
